598df40bdc0eac603ad5bd3967c07cf542b056d3
Justin van der Kwaak <justinkwaak@hotmail.com>
2024-03-04 15:28:15 +0100

    Started with first component

--- 03-pizza-menu/starter/src/App.js
+++ 03-pizza-menu/starter/src/App.js
@@ -1,8 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+function Pizza() {
+  return <div>
+    
+    <img src='pizzas/focaccia.jpg' />
+    <h2>Pizza</h2>
+  <p>Bread with italian olive oil and rosemary</p></div>
+
+}
+
 function App() {
-  return <h1>Hello React!</h1>;
+  return (<div><h1>Hello React!</h1><Pizza /></div>);
 }
 
 export default App;
